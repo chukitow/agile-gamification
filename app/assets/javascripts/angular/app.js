24f1a42b08755c2cc57219fbd9a7e70d01a8cae5
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-04 23:53:09 -0500

    configure ng route

---
+++ app/assets/javascripts/angular/app.js
@@ -0,0 +1,30 @@
+(function(){
+
+  angular
+    .module('agilegamification', [
+      'ngRoute',
+      'templates'
+    ])
+    .config(RoutesDraw);
+
+    RoutesDraw.$inject = ['$routeProvider', '$locationProvider'];
+
+    function RoutesDraw($routeProvider, $locationProvider){
+      $locationProvider.html5Mode(true);
+
+      $routeProvider
+        .when('/', {
+          templateUrl: 'home/index.html',
+          controller: 'HomeController',
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }
+
+})();
+
+
+$(document).ready(function() {
+  angular.bootstrap(document, ['agilegamification']);
+});
---
+++ app/assets/javascripts/angular/controllers/home_controller.js
@@ -0,0 +1,8 @@
+(function(){
+  angular
+    .module('agilegamification')
+    .controller('HomeController', HomeController);
+
+    function HomeController(){
+    }
+})();
