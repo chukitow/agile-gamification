5cfd57ef9255265c5ecfd9d228b9d4a1e088e450
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-04 22:52:44 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,34 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.3'
+gem 'pg'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+
+group :development, :test do
+  gem 'spring'
+  gem 'rspec-rails'
+  gem 'pry-rails'
+  gem 'faker'
+  gem 'guard-rspec'
+  gem "better_errors"
+  gem 'cucumber-api-steps', require: false
+end
+
+group :test do
+  gem 'cucumber-rails', require: false
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'shoulda-matchers'
+  gem "nyan-cat-formatter"
+  gem 'selenium-webdriver'
+  gem 'simplecov', require: false
+  gem 'codecov', require: false
+  gem 'webmock'
+  gem 'timecop'
+end
---
+++ spec/rails_helper.rb
@@ -0,0 +1,36 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'factory_girl'
+require 'simplecov'
+
+SimpleCov.start
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before(:each) do
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  Capybara.default_driver =  :webkit
+  config.include FactoryGirl::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+end
