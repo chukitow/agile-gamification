e46c14a99c2407e2518cf856297f2f11d85d51f4
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-06 17:35:57 -0500

    create crud of projects

---
+++ app/controllers/api/v1/projects_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project, only: [:show, :update, :destroy]
+  respond_to :json
+
+  def index
+    render json: current_user.projects, status: :ok
+  end
+
+  def show
+    render json: @project, status: :ok
+  end
+
+  def create
+    project = current_user.projects.new(project_params)
+
+    if project.save
+      render json: project, status: :created
+    else
+      render json: project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      render json: @project, status: :ok
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @project.destroy
+      head :no_content
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'api/auth'
+
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+    end
+  end
+
   root 'home#index'
+
+  mount_devise_token_auth_for 'User', at: 'api/auth'
   get '/*all', to: 'home#index'
 end
