671035e672648b28f46511396e31a46faf88f062
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-13 18:25:17 -0500

    create restfull comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  respond_to :json
+
+  def show
+    render json: @comment, status: :ok
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      render json: @comment, status: :ok
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @comment.destroy
+      render json: @comment, status: :ok
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :story_id)
+  end
+end
