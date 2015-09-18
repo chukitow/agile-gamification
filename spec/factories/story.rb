f475bf43897efe20c200149c3a860e585c4c6324
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-17 23:22:41 -0500

    create story with comments factory

--- spec/factories/story.rb
+++ spec/factories/story.rb
@@ -7,4 +7,11 @@ FactoryGirl.define do
     estimation  [0, 1, 2, 3, 5, 8].sample
     project
   end
+
+  factory :story_with_comments, parent: :story do
+    after(:create) do |story|
+      5.times { story.comments << create(:comment) }
+      story.save
+    end
+  end
 end
