d65d848f30d6966f2b5d5978a53fcd5e526e1eae
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-18 19:51:15 -0500

    create specs for session controler

---
+++ spec/javascripts/controllers/sessions_controller_spec.js
@@ -0,0 +1,37 @@
+(function(){
+  describe('SessionsController',function(){
+    var scope, $http, location, createController;
+
+    beforeEach(module('agilegamification'));
+
+    beforeEach(inject(function ($controller, $rootScope, $injector, $location) {
+      $http    = $injector.get('$httpBackend');
+      scope    = $rootScope.$new();
+      location = $location;
+
+      $controller('SessionsController', { $scope: scope });
+    }));
+
+    afterEach(function(){
+      $http.verifyNoOutstandingExpectation();
+      $http.verifyNoOutstandingRequest();
+    });
+
+    it('signs in the user', function (){
+      spyOn(location, 'path');
+
+      $http.whenPOST('/api/auth/sign_in')
+        .respond(200, { email: 'test@example.com' });
+
+      var user = {
+        email: 'test@example.com',
+        password: '12345678',
+      };
+
+      scope.submitLogin(user);
+      $http.flush();
+
+      expect(location.path).toHaveBeenCalledWith('/projects');
+    });
+  });
+})();
