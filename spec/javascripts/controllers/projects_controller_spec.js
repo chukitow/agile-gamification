e2ae12e283179edb5bcc44f31a3d7202950b469b
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-18 19:49:46 -0500

    create specs for project controller

---
+++ spec/javascripts/controllers/projects_controller_spec.js
@@ -0,0 +1,58 @@
+(function(){
+  describe('ProjectsController', function(){
+    var scope, $http, location, createController, Project;
+
+    beforeEach(module('agilegamification'));
+
+
+    beforeEach(inject(function ($controller, $rootScope, $injector, $location) {
+      $http    = $injector.get('$httpBackend');
+      scope    = $rootScope.$new();
+      location = $location;
+      Project  = $injector.get('Project');
+
+      $controller('ProjectsController', { $scope: scope });
+
+      var projects = [
+        { id: 1, name: 'Project example' }
+      ];
+
+      $http.whenGET('/api/v1/projects')
+        .respond(200, projects);
+
+      $http.flush();
+    }));
+
+    afterEach(function(){
+      $http.verifyNoOutstandingExpectation();
+      $http.verifyNoOutstandingRequest();
+    });
+
+    it('retrives all the projects', function(){
+      expect(scope.projects).toBeDefined();
+    });
+
+    it('create a new resource project', function(){
+      scope.openModal();
+
+      expect(scope.project).toEqual(new Project());
+    });
+
+    it('creates a new project', function(){
+      scope.openModal();
+
+      var project = {
+        name: 'Project example'
+      };
+
+      $http.whenPOST('/api/v1/projects')
+        .respond(200, project);
+
+      scope.createProject(new Project(project));
+
+      $http.flush();
+
+      expect(scope.projects.length).toEqual(2);
+    });
+  });
+})();
