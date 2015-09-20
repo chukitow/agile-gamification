948d3872310e74c41415ec635a1faac9da49aebd
Ivan Velasquez <ivan.velasquez.rios@gmail.com>
2015-09-20 14:20:32 -0500

    retrives all the states

---
+++ spec/factories/story_state.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :story_state do
+    name { Faker::Name.name }
+
+     trait :finished do
+       after(:create) do |story_state|
+         story_state.name = 'Finished'
+         story_state.save
+       end
+     end
+  end
+end
